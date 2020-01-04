95b5a5d8447ee5a96019cdcc68af5e0623bb9931
ItsLayk <layktwitch@gmail.com>
2020-01-04 13:00:40 +1100

    Init commit

---
+++ swiper.js
@@ -0,0 +1,15 @@
+class Swiper {
+    constructor(swiper, options = {}) {
+
+        // Set Wwiper target and options object
+        this.swiper = document.querySelector(swiper);
+        this.options = options;
+
+        // Kick things off
+        this.init();
+    }
+
+    init() {
+
+    }
+}
